8ec723342baff3e1a72ab83de89ff91e0365e88f
Artur <shevchenkoartur.745@gmail.com>
2021-08-25 12:49:34 +0300

    modified slider

--- js/slider.js
+++ js/slider.js
@@ -1,40 +1,17 @@
-/* Устанавливаем стартовый индекс слайда по умолчанию: */
-let slideIndex = 1;
-/* Вызываем функцию, которая реализована ниже: */
-showSlides(slideIndex);
+const showSlide = () => {
+    const slides = document.getElementsByClassName('slider__item')
+    let slideNum = 0
 
-/* Увеличиваем индекс на 1 — показываем следующий слайд: */
-function nextSlide() {
-    showSlides(slideIndex += 1);
+    setInterval(() => {
+        if(slideNum === slides.length - 1) {
+            slideNum = 0
+        }
+        slides[slideNum].classList.add('showing')
+        for(let i = 0; i < slides.length; i++) {
+            if (i !== slideNum) slides[i].classList.remove('showing')
+        }
+        slideNum++
+    }, 3000)
 }
 
-/* Уменьшаем индекс на 1 — показываем предыдущий слайд: */
-function previousSlide() {
-    showSlides(slideIndex -= 1);
-}
-
-/* Устанавливаем текущий слайд: */
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-/* Функция перелистывания: */
-function showSlides(n) {
-    /* Обращаемся к элементам с названием класса "item", то есть к картинкам: */
-    let slides = document.getElementsByClassName("slider__item");
-
-    /* Проверяем количество слайдов: */
-    if (n > slides.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-
-    /* Проходим по каждому слайду в цикле for: */
-    for (let slide of slides) {
-        slide.style.display = "none";
-    }
-    /* Делаем элемент блочным: */
-    slides[slideIndex - 1].style.display = "block";
-}
+showSlide()
